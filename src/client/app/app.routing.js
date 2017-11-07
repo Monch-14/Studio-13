65a6bba57cd35d099ca6f69fde9eb7f2947fa7a5
DelhiPanda <daniel.mtz.mu@gmail.com>
2017-11-07 08:55:41 -0600

    Se actualiza repo 07/11/17

--- src/client/app/app.routing.js
+++ src/client/app/app.routing.js
@@ -6,8 +6,8 @@
         .when('/', {
           templateUrl : 'app/home/home.html'
         })
-        .when('/banana', {
-          template: '<h1>Banana</h1><p>Bananas contain around 75% water.</p>'
+        .when('/paquetes', {
+          templateUrl : 'app/paquetes/paquetes.html'
         })
         .when('/tomato', {
           template: '<h1>Tomato</h1><p>Tomatoes contain around 95% water.</p>'
